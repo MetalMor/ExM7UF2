1a23c950cc1f3cfad6f1ddc90892d08eb890d31a
jogafe1415daw2 <jogafe1415daw2@gmail.com>
2016-01-20 15:34:23 +0100

    Exercicis M7UF2 Desenvolupament web en entorn servidor // Generació dinàmica de pàgines web

--- src/java/util/ServletUtilitats.java
+++ src/java/util/ServletUtilitats.java
@@ -1,6 +1,9 @@
 package util;
 
 /**
+ * Cosiviris varias que me he hecho para, no sé, utilizarlas
+ * Seguro que ya existe una solucion mejor, pero me aburria y me puse a hacer algo
+ * para practicar xD
  *
  * @author mor
  * 140116
@@ -9,16 +12,71 @@ public class ServletUtilitats {
     
     public static final String DOCTYPE = "<!DOCTYPE html>";
     public static final String C_R = "\n";
-    public static final String UTF_8 = "\"UTF-8\"";
-    private static final String F_POST = "\"POST\"";
-    private static final String F_GET = "\"GET\"";
-    private static final String F_PUT = "\"PUT\"";
-    public static final String POST = "POST";
-    public static final String GET = "GET";
-    public static final String PUT = "PUT";
+    public static final String UTF_8 = "UTF-8";
+    public static final String M_POST = "POST";
+    public static final String M_GET = "GET";
+    public static final String M_PUT = "PUT";
+    public static final String T_BUTTON = "button";
+    public static final String T_TEXT = "text";
+    public static final String T_SUBMIT = "submit";
     
-    public static String metode = GET;
-    public static String sortida;
+    private static String metode = M_POST;
+    private static String sortida;
+    private static String type = T_TEXT;
+    private static String value = "Envia";
+    
+    public static String filtre(String input) {
+        if (!teCaractersEspecials(input)) {
+            return (input);
+        }
+        StringBuilder filtrada = new StringBuilder(input.length());
+        char c;
+        for (int i = 0; i < input.length(); i++) {
+            c = input.charAt(i);
+            switch (c) {
+                case '<':
+                    filtrada.append("&lt;");
+                    break;
+                case '>':
+                    filtrada.append("&gt;");
+                    break;
+                case '"':
+                    filtrada.append("&quot;");
+                    break;
+                case '&':
+                    filtrada.append("&amp;");
+                    break;
+                default:
+                    filtrada.append(c);
+            }
+        }
+        return (filtrada.toString());
+    }
+    
+    private static boolean teCaractersEspecials(String input) {
+        boolean flag = false;
+        if ((input != null) && (input.length() > 0)) {
+            char c;
+            for (int i = 0; i < input.length(); i++) {
+                c = input.charAt(i);
+                switch (c) {
+                    case '<':
+                        flag = true;
+                        break;
+                    case '>':
+                        flag = true;
+                        break;
+                    case '"':
+                        flag = true;
+                        break;
+                    case '&':
+                        flag = true;
+                        break;
+                }
+            }
+        }
+        return (flag);
+    }
     
     public static String construirPagina(String titol, String contingut) {
         
@@ -43,36 +101,31 @@ public class ServletUtilitats {
         
     }
     
-    public static String escriuFormulari(String[] args) {
+    public static String escriuFormulari(int camps) {
         
-        sortida = "<form " + method(metode) + " >" + C_R;
-        for (String camp: args) {
-            sortida += camp + "<input type=\"text\" id =\"" + camp + "\" /><br/>" + C_R;
+        int i = 0;
+        String camp;
+        setType(T_TEXT);
+        
+        sortida = "<form method=" + ambCometes(metode) + " >" + C_R;
+        while (i < camps) {
+            
+            camp = "field" + camps;
+            sortida += "Nombre: <input type=" + ambCometes(type) + " name=" + 
+                    camp + " /><br/>" + C_R;
+            i++;
+            
         }
-        sortida += "</form>";
+        setType(T_SUBMIT);
+        sortida += "<input type=" + ambCometes(type) + " value=" + ambCometes(value) + " />" + C_R + 
+                "</form>";
         
         return sortida;
         
     }
     
-    private static String method(String met) {
-        
-        switch(met) {
-            case POST:
-                met = F_POST;
-                break;
-            case PUT:
-                met = F_PUT;
-                break;
-            case GET:
-                met = F_GET;
-                break;
-            default:
-                break;
-        }
-        
-        return met;
-        
+    private static String ambCometes(String s) {
+        return '\"' + s + '\"';
     }
     
     /*
@@ -81,6 +134,14 @@ public class ServletUtilitats {
     
     public static void setMetode(String m) {
         metode = m;
+    }
+    
+    public static void setType(String t) {
+        type = t;
+    }
+    
+    public static void setValue(String v) {
+        value = v;
     }
     
 }
